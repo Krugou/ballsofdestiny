cb173ba465c2647517e91ffd2c1036fe91ae3076
Krugou <59641229+Krugou@users.noreply.github.com>
2024-03-17 16:06:04 +0200

    update

---
+++ Assets/Scripts/KeepMoving.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required for UI elements
+using UnityEngine.SceneManagement; // Required for scene management
+
+public class KeepMoving : MonoBehaviour
+{
+    public GameObject ballKing; // Reference to the BallKing GameObject
+    private Vector3 lastPosition; // The last recorded position of the BallKing
+    private float timeSinceLastMove; // The time since the BallKing last moved
+    public Text gameOverText; // Reference to the Text component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the last position to the current position of the BallKing
+        lastPosition = ballKing.transform.position;
+        gameOverText.text = ""; // Ensure the game over text is empty at the start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the BallKing has moved
+        if (ballKing.transform.position != lastPosition)
+        {
+            // If the BallKing has moved, reset the timer and update the last position
+            timeSinceLastMove = 0;
+            lastPosition = ballKing.transform.position;
+        }
+        else
+        {
+            // If the BallKing hasn't moved, increment the timer
+            timeSinceLastMove += Time.deltaTime;
+
+            // If the BallKing hasn't moved for 10 seconds, display a "Game Over" message
+            if (timeSinceLastMove >= 10)
+            {
+                gameOverText.text = "Game Over";
+                timeSinceLastMove = 0; // Reset the timer to prevent the message from being displayed repeatedly
+            }
+        }
+
+        // If the R key is pressed, reload the current scene
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
